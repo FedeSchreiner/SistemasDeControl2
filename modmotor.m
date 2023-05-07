d083a19f20cfb07ee7c2b4100c231c84db289ee3
Schreiner-F <schreinerfederico1@gmail.com>
2023-05-07 19:49:03 -0300

    update

--- modmotor.m
+++ modmotor.m
@@ -1,27 +1,55 @@
+% function [X]=modmotor(t_etapa,xant,accion,Tl)
+% % Laa=366e-6;     %inductancia armadura
+% % J=5e-9;         %inercia motor
+% % Ra=55.6;        %resistencia armadura
+% B=0;            %amortiguamiento
+% % Ki=6.49e-3;     %constantes del motor
+% % Km=6.53e-3;     %contantes de motot
+% 
+% Laa=620e-6;     %inductancia armadura
+% Ra=99.24;        %resistencia armadura
+% Ki=16.52;     %constantes del motor
+% Km=0.0605;     %contantes de morot
+% J=4.1748e-6;         %inercia motor
+% 
+% TL=Tl;
+% % TL=2e-7;
+% % TL=2e-6;
+% %TL=2.12788e-5;
+% % TL=1;
+% 
+% 
+% Va=accion;   %voltaje de entrada
+% h=1e-7;      
+% omega=xant(1);
+% ia=xant(2);
+% wp=xant(3);
+%     for ii=1:t_etapa/h
+%         wpp= (-wp*(Ra*J+Laa*B)-omega*(Ra*B+Ki*Km)+Va*Ki)/(J*Laa);
+%         iap= ((-Ra*ia)-(Km*omega)+Va)/Laa;
+%         wp= wp+h*wpp;
+%         wp= wp -((1/J)*TL);
+%         omega=omega+h*wp;
+%         ia=ia+iap*h;
+%     end
+% X=[omega,ia,wp];
+
+
+% motor:
 function [X]=modmotor(t_etapa,xant,accion,Tl)
-% Laa=366e-6;     %inductancia armadura
-% J=5e-9;         %inercia motor
-% Ra=55.6;        %resistencia armadura
-B=0;            %amortiguamiento
-% Ki=6.49e-3;     %constantes del motor
-% Km=6.53e-3;     %contantes de motot
- Laa=10.76e-3;     %inductancia armadura
-Ra=99;        %resistencia armadura
-Ki=16.09;     %constantes del motor
-Km=0.0621;     %contantes de morot
-J=5.286e-6;         %inercia motor
+Laa=620e-6;     %inductancia armadura
+Ra=99.24;        %resistencia armadura
+Ki=16.52;     %constantes del motor
+Km=0.0605;     %contantes de morot
+J=4.1748e-6;         %inercia motor
+B=0;
 TL=Tl;
-% TL=2e-7;
-% TL=2e-6;
-%TL=2.12788e-5;
-% TL=1;
-
-
 Va=accion;   %voltaje de entrada
-h=1e-7;      
+h=1e-7;      % como si fuera el tiempo de simulacion
 omega=xant(1);
-ia=xant(2);
-wp=xant(3);
+wp=xant(2);
+ia=xant(3);
+thete=xant(4);
     for ii=1:t_etapa/h
         wpp= (-wp*(Ra*J+Laa*B)-omega*(Ra*B+Ki*Km)+Va*Ki)/(J*Laa);
         iap= ((-Ra*ia)-(Km*omega)+Va)/Laa;
@@ -29,32 +57,7 @@ wp=xant(3);
         wp= wp -((1/J)*TL);
         omega=omega+h*wp;
         ia=ia+iap*h;
-    end
-X=[omega,ia,wp];
-
-
-% % motor:
-% function [X]=modmotor(t_etapa,xant,accion,Tl)
-% Laa=10.76e-3;     %inductancia armadura
-% Ra=99;        %resistencia armadura
-% Ki=16.09;     %constantes del motor
-% Km=0.0621;     %contantes de morot
-% J=5.286e-6;         %inercia motor
-% B=0;
-% TL=-Tl;
-% Va=accion;   %voltaje de entrada
-% h=1e-7;      % como si fuera el tiempo de simulacion
-% omega=xant(1);
-% wp=xant(2);
-% ia=xant(3);
-% thete=xant(4);
-%     for ii=1:t_etapa/h
-%         wpp= (-wp*(Ra*J+Laa*B)-omega*(Ra*B+Ki*Km)+Va*Ki)/(J*Laa);
-%         iap= ((-Ra*ia)-(Km*omega)+Va)/Laa;
-%         wp= wp+h*wpp;
-%         omega=omega+h*wp;
-%         ia=ia+iap*h;
-%           thetep = omega;
-%         thete = thete + h*thetep;
-%       end
-% X=[omega,wp,ia,thete];
+          thetep = omega;
+        thete = thete + h*thetep;
+      end
+X=[omega,wp,ia,thete];
